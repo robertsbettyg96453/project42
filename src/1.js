da9ae6f04b40d3c8c1b44f870337021de0633f78
robertsbettyg96453 <roberts_bettyg96453@gmx.com>
2025-03-04 20:04:02 +0000

    Add 1.js

---
+++ src/1.js
@@ -0,0 +1,8 @@
+function randomNumber(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let x = randomNumber(100, 1000);
+console.log(x);
