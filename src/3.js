19bfd8111aaf6ae7bc66ea3daf8c3a438f79b3c9
robertsbettyg96453 <roberts_bettyg96453@gmx.com>
2025-03-06 18:54:31 +0000

    Add 3.js

---
+++ src/3.js
@@ -0,0 +1,8 @@
+function getRandomCode() {
+  let code = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < 5; i++) {
+    code += characters.charAt(Math.floor(Math.random() * characters.length));
+  }
+  return code;
+}
